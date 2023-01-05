8fe420cc4832619fce744015405426c08f676c91
benwilcock <benwilcock@gmail.com>
2023-01-05 17:06:35 +0000

    added the EJS template enjine and a index page

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.get('/', (req, res) => {
+//   res.send('Hello from the server!');
+// });
+
+// set the view engine to EJS
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  // render the 'index' template, and pass in a title and message
+  res.render('pages/index', { title: 'My Express App', message: 'Hello World' });
 });
 
 app.get('/health', (req, res) => {
