c6eab61bc573a5a1084e549174126e525a62804d
benwilcock <benwilcock@gmail.com>
2023-01-06 10:36:02 +0000

    added static files, fixed the api, fixed the index, added picture

--- server.js
+++ server.js
@@ -2,16 +2,33 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello from the server!');
-// });
+// enable parsing of JSON bodies in request
+app.use(express.json());
+app.use(express.static('public'));
+
+// create a list of items
+const messages = {
+  msg_subject: 'Secure software supply chains are great!',
+  msg_body: 'This is my message body.',
+  client: 'VMware',
+  framework: 'Node JS, Express, and EJS'
+};
+
+// define a route to retrieve all items
+app.get('/api/messages', (req, res) => {
+  res.json(messages);
+});
 
 // set the view engine to EJS
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   // render the 'index' template, and pass in a title and message
-  res.render('pages/index', { title: 'My Express App', message: 'Hello World' });
+  res.render('pages/index', { 
+    title: 'Powered By VMware Tanzu Application Platform', 
+    client: messages.client,
+    framework: messages.framework
+   });
 });
 
 app.get('/health', (req, res) => {
